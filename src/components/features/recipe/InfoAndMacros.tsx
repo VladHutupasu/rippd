07bf476be988a67996410529ff2d8b4e7b1208f9
VladHutupasu <vlad.hutupasu@yahoo.com>
2024-08-31 13:30:33 +0200

    feat: new protein pancakes recipe

--- src/components/features/recipe/InfoAndMacros.tsx
+++ src/components/features/recipe/InfoAndMacros.tsx
@@ -9,24 +9,24 @@ export default function InfoAndMacros({
   macrosPerServing: { calories: number; carbs: number; protein: number; fats: number };
 }) {
   return (
-    <div className="flex flex-col max-sm:pt-[20rem] justify-between gap-2">
+    <div className="flex flex-col max-sm:pt-[20rem] justify-between gap-2 w-full">
       <h1 className="text-3xl lg:text-6xl text-left font-bold text-base-content mb-2">{recipe.name}</h1>
 
       <div className="flex flex-col gap-3 sm:gap-4 xl:gap-8">
         {/* Info */}
         <div className="stats stats-horizontal font-light shadow h-20 sm:h-24 2xl:h-28 flex">
           <div className="stat px-3 sm:px-8 flex-1 min-w-24">
-            <div className="stat-title text-xs sm:text-sm xl:text-base">Calories</div>
+            <div className="stat-title font-medium text-xs sm:text-sm xl:text-base">Calories</div>
             <div className="stat-value font-bold text-lg sm:text-xl xl:text-3xl">{macrosPerServing.calories} kcal</div>
           </div>
 
           <div className="stat px-3 sm:px-8 flex-1 min-w-24">
-            <div className="stat-title text-xs sm:text-sm xl:text-base">Time</div>
+            <div className="stat-title font-medium text-xs sm:text-sm xl:text-base">Time</div>
             <div className="stat-value font-bold text-lg sm:text-xl xl:text-3xl">{recipe.cookTime} min</div>
           </div>
 
           <div className="stat px-3 sm:px-8 flex-1 min-w-24">
-            <div className="stat-title text-xs sm:text-sm xl:text-base">Complexity</div>
+            <div className="stat-title font-medium text-xs sm:text-sm xl:text-base">Complexity</div>
             <div className="stat-value font-bold text-lg sm:text-xl xl:text-3xl">{recipe.complexity}</div>
           </div>
         </div>
@@ -34,21 +34,21 @@ export default function InfoAndMacros({
         {/* Macros */}
         <div className="stats stats-horizontal font-light shadow h-20 sm:h-24 2xl:h-28 flex">
           <div className="stat px-3 sm:px-8 flex-1 min-w-24">
-            <div className="stat-title text-xs sm:text-sm xl:text-base">
+            <div className="stat-title font-medium text-xs sm:text-sm xl:text-base">
               Carbs <BoltIcon strokeWidth={2} className="h-4 w-4 inline-block" />
             </div>
             <div className="stat-value font-bold text-lg sm:text-xl xl:text-3xl">{macrosPerServing.carbs}g</div>
           </div>
 
           <div className="stat px-3 sm:px-8 flex-1 min-w-24">
-            <div className="stat-title text-xs sm:text-sm xl:text-base">
+            <div className="stat-title font-medium text-xs sm:text-sm xl:text-base">
               Protein <FireIcon strokeWidth={2} className="h-4 w-4 inline-block" />
             </div>
             <div className="stat-value font-bold text-lg sm:text-xl xl:text-3xl">{macrosPerServing.protein}g</div>
           </div>
 
           <div className="stat px-3 sm:px-8 flex-1 min-w-24">
-            <div className="stat-title text-xs sm:text-sm xl:text-base">
+            <div className="stat-title font-medium text-xs sm:text-sm xl:text-base">
               Fats <RocketLaunchIcon strokeWidth={2} className="h-4 w-4 inline-block" />
             </div>
             <div className="stat-value font-bold text-lg sm:text-xl xl:text-3xl">{macrosPerServing.fats}g</div>
